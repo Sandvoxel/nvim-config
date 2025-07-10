3b1575bed8215696ba19be18406ad969ad95d669
Gilbert Sanchez <me@gilbertsanchez.com>
2025-07-10 18:24:30 -0500

    removed some bad commands

--- lua/plugins/window-picker.lua
+++ lua/plugins/window-picker.lua
@@ -1,16 +1,9 @@
 return {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
-    version = '2.*',
-    config = function()
-        require('window-picker').setup()
-
-        vim.keymap.set("n", "<leader>w", function()
-            local picked = require("window-picker").pick_window()
-            if picked then
-                vim.api.nvim_set_current_win(picked)
-            end
-        end, { desc = "Pick and jump to window" })
-    end,
+  "s1n7ax/nvim-window-picker",
+  name = "window-picker",
+  event = "VeryLazy",
+  version = "2.*",
+  config = function()
+    require("window-picker").setup()
+  end,
 }
