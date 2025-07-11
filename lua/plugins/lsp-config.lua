d0061f08aeca11dd16a3e9a02a32ef2274ed843b
Sandvoxel <kovalchickzz@gmail.com>
2025-07-11 13:40:07 -0500

    added crate autocompleate

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,5 +1,12 @@
 return {
   {
+    "saecki/crates.nvim",
+    tag = "stable",
+    config = function()
+      require("crates").setup()
+    end,
+  },
+  {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
