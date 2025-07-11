e66515edaf6c51da8d9fa0bb8d2fe6e71fd16f69
Sandvoxel <kovalchickzz@gmail.com>
2025-07-11 12:32:23 -0500

    moved rust lsp commands to only appear in rust files

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,27 +3,7 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^5", -- Recommended
     lazy = false, -- This plugin is already lazy
-    config = function()
-      local bufnr = vim.api.nvim_get_current_buf()
-      vim.keymap.set("n", "<leader>a", function()
-        vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
-        -- or vim.lsp.buf.codeAction() if you don't want grouping.
-      end, { silent = true, buffer = bufnr })
-      vim.keymap.set(
-        "n",
-        "K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-        function()
-          vim.cmd.RustLsp({ "hover", "actions" })
-        end,
-        { silent = true, buffer = bufnr }
-      )
-
-      vim.keymap.set("n", "<leader>ci", function()
-        local buf = vim.api.nvim_get_current_buf()
-        local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = buf })
-        vim.lsp.inlay_hint.enable(not enabled, { bufnr = buf })
-      end, { desc = "Toggle Inlay Hints" })
-    end,
+    config = function() end,
   },
   {
     "williamboman/mason.nvim",
