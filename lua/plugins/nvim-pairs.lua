387444d4bc57685efdf1f29208cbdeb0a7144f15
Sandvoxel <kovalchickzz@gmail.com>
2025-07-11 13:42:16 -0500

    Fixed pairs with <>

--- lua/plugins/nvim-pairs.lua
+++ lua/plugins/nvim-pairs.lua
@@ -1,7 +1,29 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
+    local npairs = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+    local cond = require("nvim-autopairs.conds")
+
+    npairs.setup({
+      fast_wrap = {},
+      disable_filetype = { "TelescopePrompt", "vim" },
+    })
+
+    -- Add a custom rule for Rust-style generics: <...>
+    npairs.add_rule(
+      Rule("<", ">", { "-html", "-javascriptreact", "-typescriptreact" }) -- exclude HTML/JSX
+        :with_pair(cond.before_regex("%a+:?:?$", 3)) -- e.g. after Vec, Option, std::
+        :with_move(function(opts)
+          return opts.char == ">"
+        end)
+        :with_cr(cond.none()) -- don't auto-pair on carriage return
+    )
+
+    -- Integrate with nvim-cmp for proper bracket handling
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    local cmp = require("cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
 }
